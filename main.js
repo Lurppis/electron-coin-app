03363682eea286b5784d57f2cf4c3b49bf37ab41
Marcin Pilarczyk <marcinpilarczyk@me.com>
2018-02-11 15:52:17 +0100

    remove some submenus

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let addWin;
 
 function createWindow() {
 	// Create the browser window.
-	win = new BrowserWindow({ width: 800, height: 600, title: 'Todo app' });
+	win = new BrowserWindow({ width: 800, height: 600, title: 'Shop list' });
 
 	// and load the index.html of the app.
 	win.loadURL(url.format({
@@ -81,14 +81,7 @@ const template = [{
 	label: 'View',
 	submenu: [
 		{ role: 'reload' },
-		{ role: 'forcereload' },
 		{ role: 'toggledevtools' },
-		{ type: 'separator' },
-		{ role: 'resetzoom' },
-		{ role: 'zoomin' },
-		{ role: 'zoomout' },
-		{ type: 'separator' },
-		{ role: 'togglefullscreen' }
 	]
 }];
 
